c2361da2efbe1930802616b5a6005b8690acc274
foxmccloud24 <56215296+foxmccloud24@users.noreply.github.com>
2019-11-16 05:45:36 -0800

    Add files via upload

---
+++ gmc/js/app.js
@@ -0,0 +1,40 @@
+/*prompt("comment ca va ?");*/
+
+ /*var x=5
+ var y=3
+ var z=2
+ alert(x+y);*/
+ 
+
+ /*var myString="hello world"
+ var secondString="world"
+ alert(myString.toLowerCase());
+ alert(secondString.toUpperCase());*/
+
+/* var myString="hello there, this is a split text"
+ var parts=myString.split(",");
+ alert(parts);*/
+ 
+ /*var a=5
+ var b=2
+ var c=5
+ alert(a==b);
+ alert(a==c);*/
+ 
+ /*var i;
+ for(i=1;i<3;i++){
+	alert(i);
+ }*/
+
+/*var myArray=[1,2,3,4];
+var otherArray=["1","2","3"];
+console.log(myArray)*/
+
+/*var myArray=[1,2,3,4];
+alert(myArray[2]) //Returns 3*/
+
+/*function mean(a,b,c){
+	return((a+b+c)/4);
+}
+alert(mean(3,5,7));*/
+
